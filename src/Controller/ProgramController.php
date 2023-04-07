f3e1782cf40606f54366a64936291a4d0b930af7
x0-3 <steph6363@gmail.com>
2023-04-07 16:01:49 +0200

    add delete edit module

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,6 +6,7 @@ use App\Entity\Category;
 use App\Entity\Module;
 use App\Entity\Program;
 use App\Form\CategoryType;
+use App\Form\ModuleType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,9 +29,10 @@ class ProgramController extends AbstractController
     }
 
 
+    // ********************************************* add edit and delete a category *********************** //
     #[Route('/program/add', name: 'add_category')]
     #[Route('/program/{id}/edit', name: 'edit_category')]
-    public function add(ManagerRegistry $docrine, Category $category = null, Request $request)
+    public function addCategory(ManagerRegistry $docrine, Category $category = null, Request $request)
     {
 
         // if the entreprise id doesn't exist then create it
@@ -77,7 +79,65 @@ class ProgramController extends AbstractController
 
     }
 
+    // ********************************************* add edit and delete a module *********************** //
+    #[Route('/program/addModule', name: 'add_module')]
+    #[Route('/program/{id}/editModule', name: 'edit_module')]
+    public function addModule(ManagerRegistry $docrine, Module $module = null, Request $request)
+    {
 
+        // if the entreprise id doesn't exist then create it
+        if (!$module) {
+            $module = new Module();
+        }
+        // else edit
+
+        // create a form that refers to the builder in employeType
+        $form = $this->createForm(ModuleType::class, $module);
+        $form ->handleRequest($request); //analyse whats in the request / gets the data
+
+        // if the form is submitted and check security 
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $module = $form->getData(); // get the data submitted in form and hydrate the object 
+
+            // need the doctrine manager to get persist and flush
+            $entityManager = $docrine->getManager(); 
+            $entityManager->persist($module); // prepare
+            $entityManager->flush(); // execute
+
+            // redirect to list employe
+            return $this->redirectToRoute('app_module');
+        }
+
+        // vue to show form
+        return $this->render('program/addModule.html.twig', [
+
+            'formAddModule'=> $form->createView(),   
+            'edit'=> $module->getId(),   
+        ]);
+    }
+
+    #[Route('/program/{id}/deleteModule', name: 'delete_module')]
+    public function deleteModule(ManagerRegistry $docrine, Module $module):Response
+    {
+        $entityManager = $docrine->getManager();
+        $entityManager->remove($module); //remove in object
+        $entityManager->flush(); // send the request to the db 
+
+        return $this->redirectToRoute('app_module');
+
+    }
+
+
+
+
+
+
+
+
+
+
+    // 
     #[Route('/program/{id}', name: 'detail_module')]
     public function detail(Module $module, Category $category):Response
     {
---
+++ src/Form/ModuleType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Module;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ModuleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name',TextType::class)
+            ->add('category', EntityType::class,[
+                'class'=>Category::class,
+                'choice_label' => 'name'])
+                ->add('submit',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Module::class,
+        ]);
+    }
+}
